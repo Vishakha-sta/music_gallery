a0a4f6e5920d28e4c37d4ec11e1b81beee19d960
Vishakha-sta <vishakhagandhi749@gmail.com>
2021-07-13 16:37:25 +0530

    working mode on i am doing it :p.....

--- user_dashboard.php
+++ user_dashboard.php
@@ -4,13 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/user_dashboard.css">
     <title>Dashboard</title>
 </head>
 <body>
     <div class="dashboard">    
-        <div class="songs">
+        <div class="songs dash-display">
             <div class="heading">
-                <h2>Songs</h2>
+                <h2><i class="fas fa-guitar"></i> Songs</h2>
+            </div>
+            <div class="song_card ">
+                <?php
+                    include 'song_cards.php';
+                ?>
+            </div>
+        </div>
+        <div class="album dash-display">
+            <div class="heading">
+                <h2><i class="fas fa-drum"></i> Albums</h2>
             </div>
             <div class="song_card">
                 <?php
@@ -18,9 +29,9 @@
                 ?>
             </div>
         </div>
-        <div class="album">
+        <div class="artist dash-display">
             <div class="heading">
-                <h2>Albums</h2>
+                <h2><i class="fas fa-compact-disc"></i> Artists</h2>
             </div>
             <div class="song_card">
                 <?php
@@ -28,19 +39,9 @@
                 ?>
             </div>
         </div>
-        <div class="artist">
+        <div class="playlist dash-display">
             <div class="heading">
-                <h2>Artists</h2>
-            </div>
-            <div class="song_card">
-                <?php
-                    include 'song_cards.php';
-                ?>
-            </div>
-        </div>
-        <div class="playlist">
-            <div class="heading">
-                <h2>Playlist</h2>
+                <h2><i class="fas fa-music"></i> Playlist</h2>
             </div>
             <div class="song_card">
                 <?php
